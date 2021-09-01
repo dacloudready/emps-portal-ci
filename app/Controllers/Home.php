e094edd7a0fe062e3a4c22936196b76a192ffe0c
dlavergonzado <da.cloudready@gmail.com>
2021-09-01 18:43:27 +0800

    initial upload

---
+++ app/Controllers/Home.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Timelog;
+
+class Home extends BaseController
+{
+	private $timelog;
+
+	function __construct(){
+		$this->timelog = new Timelog();
+	}
+
+	public function index()
+	{
+		$data['logs'] = $this->timelog->findAll('100'); 
+		return view('main', $data);
+	}
+
+
+
+
+
+}
